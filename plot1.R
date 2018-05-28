482bee2423ab02ca64d0ae6b09de870942ee1293
AbiramiSoundar <amy.soundar@gmail.com>
2018-05-28 07:52:51 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+setwd("C://EPC")
+dataset <- read.table ("./household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors=F)
+dataset$Date <- as.Date(dataset$Date, format="%d/%m/%Y")
+data <- subset (dataset, subset=(Date >= "2007-02-01" & Date<="2007-02-02"))
+png(filename="plot1.png", width=480, height=480, units="px")
+hist (data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png, "plot1.png",width=480, height=480)
+dev.off()
